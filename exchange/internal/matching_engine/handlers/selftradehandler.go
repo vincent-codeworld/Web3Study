59017b6814bb1b7ec6d4ef99e74096d9415bb0b5
Vincent <vincentlee363787493@gmail.com>
2026-01-30 18:21:31 +0800

    selftradehandler.go

--- exchange/internal/matching_engine/handlers/selftradehandler.go
+++ exchange/internal/matching_engine/handlers/selftradehandler.go
@@ -6,6 +6,22 @@ import (
 
 func SelfTradeHandler(taker *dto.Order, maker *dto.Order) {
 	switch taker.Stp {
+	case dto.SelfTradeWMType_STP_AST:
 
+	case dto.SelfTradeWMType_STP_DC:
+
+	case dto.SelfTradeWMType_STP_CO:
+		maker.State = dto.OrderState_ORDER_STATE_CANCELED
+		if maker.State == dto.OrderState_ORDER_STATE_PARTIAL_FILLED {
+			maker.State = dto.OrderState_ORDER_STATE_PARTIAL_CANCELED
+		}
+	case dto.SelfTradeWMType_STP_CN:
+		taker.State = dto.OrderState_ORDER_STATE_CANCELED
+	case dto.SelfTradeWMType_STP_CB:
+		taker.State = dto.OrderState_ORDER_STATE_CANCELED
+		maker.State = dto.OrderState_ORDER_STATE_CANCELED
+		if maker.State == dto.OrderState_ORDER_STATE_PARTIAL_FILLED {
+			maker.State = dto.OrderState_ORDER_STATE_PARTIAL_CANCELED
+		}
 	}
 }
